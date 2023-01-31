d39dc309d581522be7298e55c702a9d39d476939
Magdalena Senger <magdalenasenger@MacBook-Air-Mac.local>
2023-01-31 11:50:22 +0100

    add search

--- vite-project/src/main.ts
+++ vite-project/src/main.ts
@@ -1,10 +1,11 @@
 import '../css/style.css'
 
 const container = document.querySelector<HTMLDivElement>('.container');
+const select = document.querySelector<HTMLSelectElement>('#countries');
 
-// get country data
+// start page
 const arrCountries: string[] = ['germany', 'usa', 'brazil', 'iceland', 'afghan', 'Åland', 'albania', 'algeria'];
-type Test = { 
+type Data = { 
   flags: {
     svg: HTMLImageElement;
   }
@@ -16,7 +17,7 @@ type Test = {
   capital: string[];
 }[]
 
-const dataBox = (json: Test) => {
+const dataBox = (json: Data) => {
   return `
     <section class="data__box">
        <img src=${json[0].flags.svg} class="data__box--flag" />
@@ -33,35 +34,56 @@ arrCountries.map((country) => {
   .then((response) => response.json())
   .then((json) => {
     if(container) {
-      container.innerHTML+= dataBox(json);
+      container.innerHTML += dataBox(json);
     }
   })
 })
 
+// select
+interface DataAll { 
+  flags: {
+    svg: HTMLImageElement;
+  }
+  name: { 
+    common: string; 
+  };
+  population: number;
+  region: string;
+  capital: string[];
+}[]
 
+const dataBoxAll = (json: DataAll) => {
+  return `
+    <section class="data__box">
+       <img src=${json.flags.svg} class="data__box--flag" />
+       <div class="data__box--name">${json.name.common}</div>
+       <div class="data__box--details">${json.population}</div>
+       <div class="data__box--details">${json.region}</div>
+       <div class="data__box--details">${json.capital}</div>
+    </section>
+    `
+}
 
+const selectFn = (output: string) => {
+  fetch(`https://restcountries.com/v3.1/region/${output}`)
+.then((response) => response.json())
+.then((json:[]) => {
+  if(container) {
+    json.map(el => {
+      container.innerHTML += dataBoxAll(el);
+    })
+  }
+})
+}
 
+if(select) {
+  select.addEventListener('change', function() {
+    const output = select.options[select.selectedIndex].value;
+    if (container) {
+      container.innerHTML = '';
+    }
+    selectFn(output);
+  })
+}
 
-
-
-
-
-// document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-//   <div>
-//     <a href="https://vitejs.dev" target="_blank">
-//       <img src="/vite.svg" class="logo" alt="Vite logo" />
-//     </a>
-//     <a href="https://www.typescriptlang.org/" target="_blank">
-//       <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-//     </a>
-//     <h1>Vite + TypeScript</h1>
-//     <div class="card">
-//       <button id="counter" type="button"></button>
-//     </div>
-//     <p class="read-the-docs">
-//       Click on the Vite and TypeScript logos to learn more
-//     </p>
-//   </div>
-// `
-
-// setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+// input (search)
