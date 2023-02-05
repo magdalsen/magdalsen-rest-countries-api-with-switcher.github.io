2b726b218d002ddd920a36e777a8162e0ec2c867
Magdalena Senger <magdalenasenger@MacBook-Air-Mac.local>
2023-02-05 21:42:54 +0100

    deploy file update

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-    base: '/magdalsen-rest-countries-api-with-switcher/'
+    base: '/magdalsen-rest-countries-api-with-switcher.github.io/'
 })
